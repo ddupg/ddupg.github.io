eabc5770bd91bc1be608eb0ac19606a8e07e5ee9
Xin Sun <ddupgs@gmail.com>
2023-01-12 14:10:43 +0800

    #8 [导航]插图

--- src/components/Navigation/data.tsx
+++ src/components/Navigation/data.tsx
@@ -107,6 +107,11 @@ const navigationData: Classify[] = [
         url: "https://www.tailwindcss.cn/",
         intro: "Tailwind CSS 是一个功能类优先的 CSS 框架，它集成了诸如 flex, pt-4, text-center 和 rotate-90 这样的的类，它们能直接在脚本标记语言中组合起来，构建出任何设计。",
       },
+      {
+        name: "manypixels插图素材库",
+        url: "https://www.manypixels.co/gallery",
+        intro: "插画插图素材库免费下载，支持SVG/PNG",
+      },
     ],
   },
   {
